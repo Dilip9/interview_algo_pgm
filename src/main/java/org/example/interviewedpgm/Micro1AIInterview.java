ca6e3e678f224dc269b1470e83bcaa727f4074d0
Dilip9 <dilip.verma940805@gmail.com>
2025-03-15 02:19:41 +0530

    micro1AI based interview

--- src/main/java/org/example/interviewedpgm/Micro1AIInterview.java
+++ src/main/java/org/example/interviewedpgm/Micro1AIInterview.java
@@ -1,4 +1,27 @@
 package org.example.interviewedpgm;
 
+import java.util.Scanner;
+
 public class Micro1AIInterview {
+    // input str = "abc#d#e"
+    // output = "abe";
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter String which consists # included : ");
+        String str = scn.nextLine();
+        System.out.println("Output String : " + removeHashFromString(str));
+    }
+    public static String removeHashFromString(String str){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == '#'){
+                if(sb.length() > 0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
 }
