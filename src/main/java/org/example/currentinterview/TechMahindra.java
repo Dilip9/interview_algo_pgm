e72aefbd864f399f252c199daf010c676379d972
Dilip9 <dilip.verma940805@gmail.com>
2025-03-15 10:30:40 +0530

    string related oprations added

--- src/main/java/org/example/currentinterview/TechMahindra.java
+++ src/main/java/org/example/currentinterview/TechMahindra.java
@@ -11,6 +11,14 @@ public class TechMahindra {
 
         int [] arr = {3,0,2,0,0,5,4,0,1,0};
         sortNumberwithaddingZeroAtLast(arr);
+        // reverse String.
+        String str = "Hello my dear";
+        reverseString(str);
+        reverseStringUsingStream(str);
+        String strs = "Hello my dear";
+        reverseStringWordsOnly(strs);
+        String strsing = "Hello my dear daughter";
+        Arrays.stream(strsing.split(" ")).map(s -> new StringBuffer(s).reverse()).forEach(ch -> System.out.printf("%s ",ch));
 
     }
     public static void sortNumberwithaddingZeroAtLast(int [] unsortedarr){
@@ -30,5 +38,30 @@ public class TechMahindra {
 
     }
 
+    public static void reverseString(String str){
+        char[] ch = str.toCharArray();
+        StringBuffer sb = new StringBuffer();
+        for(int i=ch.length-1;i>=0;i--){
+           sb.append(ch[i]);
+        }
+        System.out.println(sb.toString());
+    }
+    public static void reverseStringUsingStream(String str){
+        Stream.of(str).map(s -> new StringBuilder(s).reverse()).forEach(System.out::println);
+    }
+
+    public static void reverseStringWordsOnly(String str){
+        String [] words = str.split(" ");
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i< words.length;i++){
+            for(int j=words[i].length()-1;j>=0;j--){
+                sb.append(words[i].charAt(j));
+            }
+            if (i < words.length - 1) {
+                sb.append(" ");
+            }
+        }
+        System.out.println(sb.toString());
+    }
 
 }
