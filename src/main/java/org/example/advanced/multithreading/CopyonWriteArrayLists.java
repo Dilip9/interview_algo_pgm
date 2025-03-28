c36f96120f40057abf000e74fba0b7d002f839f2
Dilip9 <dilip.verma940805@gmail.com>
2025-03-29 01:02:57 +0530

    Multithreading studies- copyonwriteArraylist

--- src/main/java/org/example/advanced/multithreading/CopyonWriteArrayLists.java
+++ src/main/java/org/example/advanced/multithreading/CopyonWriteArrayLists.java
@@ -1,4 +1,25 @@
 package org.example.advanced.multithreading;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class CopyonWriteArrayLists {
+
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+        list.add("Subscriber 1 ");
+        list.add("Subscriber 2");
+
+        Runnable writer  = () -> {
+            list.add(" Subscriber 3");
+            System.out.println("Added subscriber 3 using writer");
+        };
+        Runnable reader = () -> {
+            for (int i=0;i<list.size();i++){
+                System.out.println("Reader: "+list.get(i));
+            }
+        };
+        new Thread(writer).start();
+        new Thread(reader).start();
+    }
+
 }
