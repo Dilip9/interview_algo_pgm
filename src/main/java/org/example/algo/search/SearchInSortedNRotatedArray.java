2a440e9b82419c716eb677589859817f979d3404
Dilip9 <dilip.verma940805@gmail.com>
2025-03-31 00:37:55 +0530

    Binary Search - advanced search in sorted and rotated array.

---
+++ src/main/java/org/example/algo/search/SearchInSortedNRotatedArray.java
@@ -0,0 +1,35 @@
+package org.example.algo.search;
+
+public class SearchInSortedNRotatedArray {
+
+    public static void main(String[] args) {
+        int[] nums = {3,6,9,12,15,78,90, 1, 2,3};
+        int target = 1;
+        int position = positionBinarySearch(nums, target);
+        System.out.println(position);
+    }
+
+    private static int positionBinarySearch(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid+1;
+            if(nums[low] <= nums[mid]){
+                if(nums[low] < target && nums[mid]>target){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                if(nums[mid] < target && nums[high] > target){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+}
---
+++ src/main/java/org/example/ds/linkedList/ReverseSingleLL.java
@@ -0,0 +1,4 @@
+package org.example.ds.linkedList;
+
+public class ReverseSingleLL {
+}
