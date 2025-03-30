d9f11c9ff924cd2a6f465c79c65e9c94d1111686
Dilip9 <dilip.verma940805@gmail.com>
2025-03-31 00:15:27 +0530

    Binary Search - advanced search to find first and last position of element

--- src/main/java/org/example/algo/search/SearchFirstAndLastPosition.java
+++ src/main/java/org/example/algo/search/SearchFirstAndLastPosition.java
@@ -3,24 +3,54 @@ package org.example.algo.search;
 public class SearchFirstAndLastPosition {
     public static void main(String[] args) {
         int[] nums = {3,6,9,9,9,12,15,78,90};
-        int target = 15;
+        int target = 9;
         int [] result = positionOfFirstAndLast(nums, target);
+        System.out.println("First Position: "+result[0]+" Last Position: "+result[1]);
 
 
     }
 
     private static int[] positionOfFirstAndLast(int[] nums, int target) {
-        int first = findFirstElementPosition(nums, target);
-        int last = findLastElementPosition(nums, target);
+        int last = findFirstElementPosition(nums, target);
+        int first = findLastElementPosition(nums, target);
         return new int[]{first,last};
     }
 
     private static int findLastElementPosition(int[] nums, int target) {
-        return 8;
+        int low = 0, high = nums.length-1;
+        int start =-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target) {
+                start = mid;
+                high = mid-1;
+            } else if (nums[mid] < target) {
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return start;
+        //return 8;
     }
 
     private static int findFirstElementPosition(int[] nums, int target) {
-        return 6;
+        int low = 0, high = nums.length-1;
+        int last = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target) {
+                //System.out.println(" Found target element last position: " + mid+1);
+                last = mid;
+                low = mid+1;
+
+            } else if (nums[mid] < target) {
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return last;
     }
 
 
