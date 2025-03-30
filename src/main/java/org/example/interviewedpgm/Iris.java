f23b626ade392adaa097d29195d4d2df6b7caec6
Dilip9 <dilip.verma940805@gmail.com>
2025-03-31 01:26:04 +0530

    Irish Second round interview

--- src/main/java/org/example/interviewedpgm/Iris.java
+++ src/main/java/org/example/interviewedpgm/Iris.java
@@ -10,6 +10,10 @@ public class Iris {
     //output: 2,6,8,-1,2,7,-1,9,4,5
     public static void main(String[] args) {
         int[] num = {1, 2, 6, 8, 2, 7, 3, 9, 4, 5};
+        String s1 = "listen";
+        String s2 = "silent";
+        boolean isAnagram = testTwoStringIsAnagram(s1, s2);
+        System.out.println(isAnagram);
         int[] result = printNextGreaterElement(num);
         System.out.println("::"+ Arrays.toString(result));
     }
@@ -24,5 +28,13 @@ public class Iris {
             stack.push(i);
         }
         return result;
+    }
+
+    public static boolean testTwoStringIsAnagram(String s1, String s2){
+        char[] chr1 = s1.toCharArray();
+        Arrays.sort(chr1);
+        char[] chr2 = s2.toCharArray();
+        Arrays.sort(chr2);
+        return Arrays.equals(chr1, chr2);
     }
 }
