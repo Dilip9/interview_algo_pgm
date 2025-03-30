ad2d2b965b596c0d6bad795b78c94d2812d165ae
Dilip9 <dilip.verma940805@gmail.com>
2025-03-30 23:49:07 +0530

    Binary Search

--- src/main/java/org/example/algo/search/BinarySerachAlgo.java
+++ src/main/java/org/example/algo/search/BinarySerachAlgo.java
@@ -1,4 +1,16 @@
 package org.example.algo.search;
 
 public class BinarySerachAlgo {
+    public static void main(String[] args) {
+        int[] nums = {3,6,9,12,15,78,90};
+        int target = 15;
+        binarySearch(nums, target);
+    }
+    public static void binarySearch(int[] nums, int target){
+
+        int low = 0, high = nums.length-1;
+        while(low <= high){
+
+        }
+    }
 }
