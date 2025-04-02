7568f379fe4edf3fc4f4dc52f55c4eeb015bf122
Dilip9 <dilip.verma940805@gmail.com>
2025-04-02 23:48:15 +0530

    Accenture Interview Questions

---
+++ src/main/java/org/example/interviewedpgm/Accenture.java
@@ -0,0 +1,64 @@
+package org.example.interviewedpgm;
+
+public class Accenture {
+
+//
+//    List numbers = Arrays.asList(1, 2, 2, 3, 4, 4);
+//number.stream().distinct().collect(Collectors.toList());
+//    List names = Arrays.asList("Charlie", "Alice", "Bob");
+//names.stream().sort().forEach(System.out::println());
+//
+//    List numbers = Arrays.asList(5, 3, 1, 4, 2);
+//number.stream().sort().findFirst();  // first number in ascending order
+//number.stream().sort().skip(n-2).findFirst(); // third number in ascending order
+//
+//
+//
+//
+//    can u list of streams methods
+//
+//    what u know
+//    V C VAMSI KRISHNA MANCHIKANTI
+//
+//    List numbers = Arrays.asList(1, 2, 2, 3, 4, 4);
+//
+//    to print distinct elements from a list of integers.
+//
+//            streams
+//
+//    List names = Arrays.asList("Charlie", "Alice", "Bob");
+//
+//    to print names sorted in alphabetical order from a list.
+//
+//    Write a Java 8 program to find and print the first element in sorted order.
+//import java.util.Arrays;
+//
+//    List numbers = Arrays.asList(5, 3, 1, 4, 2);
+//    a has run
+//    b has run
+//
+//    c has one run
+//
+//    b extends a
+//
+//    c extend b
+//
+//    which method will execute
+//
+//    String s1 = "abc"
+//
+//    String s2 = new String "abc"
+//    s1 == s2 is it true or false ?
+//
+//            s1.equals(s2) is it true or false
+//
+//    hashmap collision
+//
+//    bank class having his own rate of interest
+//
+//            RBI - his own interest
+//
+//    ICICI bank , HDFC bank, SBI --- their own interest rate
+
+
+}
