41fae1cb9b14f733c1e4a7bdea501ceb96bcaa27
Dilip9 <dilip.verma940805@gmail.com>
2025-04-09 23:34:39 +0530

    Accolite 2nd round interview questions

--- src/main/java/org/example/algo/search/SearchInSortedNRotatedArray.java
+++ src/main/java/org/example/algo/search/SearchInSortedNRotatedArray.java
@@ -30,6 +30,4 @@ public class SearchInSortedNRotatedArray {
         }
         return -1;
     }
-
-
 }
--- src/main/java/org/example/interviewedpgm/Accolite.java
+++ src/main/java/org/example/interviewedpgm/Accolite.java
@@ -26,6 +26,9 @@ public class Accolite {
         int target = 15;
         int[] result = consicutivieSum(arr, target);
         Arrays.stream(result).forEach(System.out::println);
+        int[] binData = {4, 5, 6, 7, 1, 1, 2};
+        int targets = 1;
+        System.out.println("result : " + searchInSortedAndRotatedArray(binData, targets));
     }
 
     public static int[] consicutivieSum(int[] arr, int target) {
@@ -41,6 +44,34 @@ public class Accolite {
             }
         }
         return new int[]{-1, -1}; // Not found
+    }
+
+    // search in sorted and rotated array
+    public static int searchInSortedAndRotatedArray(int[] arr, int target){
+        if(arr == null || arr.length == 0) {
+            return -1;
+        }
+        int left =0, right = arr.length -1;
+        while(left<right){
+            int mid = left + (right -left)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(arr[left] <= arr[mid]){
+                if(arr[left] <= target && arr[mid] >= target){
+                    right = mid+1;
+                }else{
+                    left = mid+1;
+                }
+            }else{
+                if(arr[mid] <= target && arr[right] >= target){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
     }
 
 }
