a6e97a8601172635f8eb2fd77f33f3ffad031c4a
Dilip9 <dilip.verma940805@gmail.com>
2025-04-21 05:34:20 +0530

    Epam Interview Problem

--- src/main/java/org/example/interviewedpgm/Epam.java
+++ src/main/java/org/example/interviewedpgm/Epam.java
@@ -1,4 +1,26 @@
 package org.example.interviewedpgm;
 
+import java.util.HashSet;
+
 public class Epam {
+    public static void main(String[] args) {
+        String s = "abcabcabd"; // O/P: 4
+        int n = s.length();
+        int left=0, right = 0;
+        int max = 0;
+        HashSet<Character> set = new HashSet<>();
+        while(right<n){
+
+            if(!set.contains(s.charAt(right))){
+                set.add(s.charAt(right));
+                max = Math.max(max, right-left+1);
+                right++;
+            }
+            else{
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        System.out.println(max);
+    }
 }
