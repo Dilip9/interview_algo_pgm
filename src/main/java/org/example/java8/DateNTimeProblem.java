2b890fcf45c64c1c03d9c5d82fc710760cb6e9fa
Dilip9 <dilip.verma940805@gmail.com>
2025-05-28 06:06:44 +0530

    Date and Time related Programming problem.

--- src/main/java/org/example/java8/DateNTimeProblem.java
+++ src/main/java/org/example/java8/DateNTimeProblem.java
@@ -1,8 +1,13 @@
 package org.example.java8;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DateNTimeProblem {
 
@@ -18,6 +23,15 @@ public class DateNTimeProblem {
         employees.stream().filter(e -> e.getCreatedAt().isBefore(sixMonthsAgo))
                 .forEach(e -> System.out.println("Employee: " + e.getName() + ", Salary: " + e.getSalary() + ", Department: " + e.getDepartment() + ", Created At: " + e.getCreatedAt()));
 
-
+        List<LocalDate> holidays = Arrays.asList(
+                LocalDate.of(2025, 1, 1),
+                LocalDate.of(2025, 12, 31)
+        );
+        LocalDate startDate = LocalDate.of(2025, 1, 1);
+        LocalDate endDate = LocalDate.of(2025, 12, 31);
+        long result= startDate.datesUntil(endDate.plusDays(1))
+                .filter(d -> !(d.getDayOfWeek() == DayOfWeek.SATURDAY || d.getDayOfWeek()== DayOfWeek.SUNDAY || holidays.contains(d))).count();
+        System.out.println("Number of working days between " + startDate + " and " + endDate + ": " + result);
+        IntStream.rangeClosed(1, 12).mapToObj(month -> YearMonth.of(2025, month).atDay(1).with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY))).collect(Collectors.toList()).forEach(System.out::println);
     }
 }
